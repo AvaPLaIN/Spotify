0f1ddff91162cab099a6dc1f752058855461ee05
AvaSama <kevin.voss99@gmail.com>
2021-11-15 02:30:35 +0100

    fix

--- frontend/src/components/player/Player.js
+++ frontend/src/components/player/Player.js
@@ -20,7 +20,19 @@ import { PlayerComponent } from './Player.styled';
 const Player = ({ accessToken, uri }) => {
   return (
     <PlayerComponent>
-      <SpotifyPlayer play={true} token={accessToken} uris={[uri]} />
+      <SpotifyPlayer
+        autoplay
+        play={true}
+        token={accessToken}
+        uris={[uri]}
+        styles={{
+          bgColor: '#706b6b',
+          color: '#fff',
+          sliderColor: 'grey',
+          trackArtistColor: '#fff',
+          trackNameColor: '#fff',
+        }}
+      />
     </PlayerComponent>
   );
 };
